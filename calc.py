f1fac2937c187bc80474c89f7de8e9e4814af69b
Antonio Afonso Dourado Filho <tonnydourado@gmail.com>
2013-04-11 10:21:09 -0300

    Calculation logic abstracted in Calc class.

--- calc.py
+++ calc.py
@@ -1,4 +1,33 @@
 #!/usr/bin/env python3
+
+class Calc(dict):
+    def __init__(self, quit_command, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.stack = []
+        self.quit = quit_command
+
+    def clean_stack(self):
+        self.stack = []
+
+    def ops(self):
+        return self.keys()
+
+    def solve(self, input_stack):
+        while len(input_stack) > 0:
+            op = input_stack.pop(0)
+            if op == self.quit:
+                self.clean_stack()
+                return
+            if op in self.keys():
+                arg1 = self.stack.pop(0)
+                arg2 = self.stack.pop(0)
+                self.stack.insert(0, self[op](arg1, arg2))
+            else:
+                self.stack.insert(0, op)
+        result = self.stack[::]
+        self.clean_stack()
+        return result
+
 if __name__ == '__main__':
     ops = {
         '+': lambda x, y: x + y,
@@ -6,32 +35,35 @@ if __name__ == '__main__':
         '*': lambda x, y: x * y,
         '/': lambda x, y: x / y
     }
+    calc = Calc('quit', ops)
+
     
     while True:
-        stack = []
         raw_input = input(':')
-        if raw_input == 'quit':
-            break
-        else:
-            tokens = raw_input.split(' ')
-            while len(tokens) > 0:
-                token = tokens.pop(0)
-                if token in ops.keys():
-                    arg1 = stack.pop(0)
-                    arg2 = stack.pop(0)
-                    stack.insert(0, ops[token](arg1, arg2))
-                else:
+        tokens = raw_input.split(' ')
+        input_stack = []
+        for token in tokens:
+            if token == calc.quit:
+                input_stack.append(token)
+            elif token in calc.ops():
+                input_stack.append(token)
+            else:            
+                try:
+                    number = int(token)
+                except ValueError:
                     try:
-                        number = int(token)
+                        number = float(token)
                     except ValueError:
-                        try:
-                            number = float(token)
-                        except ValueError:
-                            print('ValueError: invalid number: ' + token)
-                            break
-                    stack.insert(0, number)
+                        print('ValueError: invalid number: ' + token)
+                        break
+                input_stack.append(number)
+        else:
+            result = calc.solve(input_stack)
+            if result is None:
+                break
             else:
-                print(stack)
+                print(result)
+                                
             
             
         
