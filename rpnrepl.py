1c1220d2ec5191a26978f540ca26efd3c24d88c7
Antonio Afonso Dourado Filho <tonnydourado@gmail.com>
2013-04-15 02:21:33 -0300

    Reimplementing RPNCalc (still failing).

--- rpnrepl.py
+++ rpnrepl.py
@@ -26,74 +26,16 @@ class Stack(list):
             self.pop()
 
 
-class RPNREPL(dict):
-    def __init__(self, quit_command, *args, **kwargs):
+class RPNCalc(dict):
+    def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.stack = Stack()
-        self.quit = quit_command
 
     def ops(self):
         return self.keys()
 
     def solve(self, input_stack):
-        while len(input_stack) > 0:
-            op = input_stack.pop()
-            if op == self.quit:
-                self.stack.push(self.quit)
-                break
-            if op in self.ops():
-                print(self.stack.pop(2))
-                self.stack.push(op)
-            else:
-                self.stack.push(op)
-        result = self.stack[::]
-        self.stack.empty()
-        return result
-
-    def parse(self, raw_input):
-        def to_number(token):
-            try:
-                return int(token)
-            except ValueError:
-                try:
-                    return float(token)
-                except ValueError:
-                    return None
-
-        is_op = lambda token: token in self.ops()
-        is_quit = lambda token: token == self.quit
-
-        tokens = raw_input.split(' ')
-        input_stack = Stack()
-        for token in tokens:
-            if is_op(token) or is_quit(token):
-                input_stack.push(token)
-            else:
-                number = to_number(token)
-                if number is not None:
-                    input_stack.push(number)
-                else:
-                    raise ValueError('Invalid number: ' + token)
-        return input_stack
-
-    def run(self):
-        while True:
-            raw_input = input(': ')
-            try:
-                result = self.solve(self.parse(raw_input))
-            except ValueError as e:
-                print(e)
-                raise e
-                continue
-            except IndexError:
-                print('Mal formed expression')
-                continue
-            if self.quit in result:
-                result.pop(result.index(calc.quit))
-                break
-            print(result)
-        print(result)
-
+        return []
 
 if __name__ == '__main__':
     ops = {
@@ -105,5 +47,4 @@ if __name__ == '__main__':
         '%': lambda x, y: x % y,
         '^': lambda x, y: x ** y
     }
-    calc = RPNREPL('quit', ops)
-    calc.run()
+    calc = RPNCalc(ops)
