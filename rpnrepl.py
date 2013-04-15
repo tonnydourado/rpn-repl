536fb2bcbad5df35f4532723f835d4d7b5c3a6e1
Antonio Afonso Dourado Filho <tonnydourado@gmail.com>
2013-04-15 14:53:24 -0300

    Corrected Stack.pop to match proper stack behavior and RPNCalc.solve now passes basic test.

--- rpnrepl.py
+++ rpnrepl.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 from collections import namedtuple
+from math import sqrt
 
 
 Operator = namedtuple('Operator', ['function', 'arity'])
@@ -8,9 +9,8 @@ Operator = namedtuple('Operator', ['function', 'arity'])
 class Stack(list):
     """Stack class."""
     def pop(self, n=1):
-        result = self[-(n):]
-        del self[-(n):]
-        return result
+        result = [super(Stack, self).pop() for i in range(n)]
+        return result if len(result) > 1 else result.pop()
 
     def push(self, value):
         self.append(value)
@@ -35,16 +35,38 @@ class RPNCalc(dict):
         return self.keys()
 
     def solve(self, input_stack):
-        return []
+        while input_stack.size() > 0:
+            value = input_stack.pop()
+            if value in self.ops():
+                op = self[value].function
+                args = self.stack.pop(self[value].arity)
+                args = args if isinstance(args, list) else [args]
+                self.stack.push(op(*args))
+            else:
+                self.stack.push(value)
+        result = self.stack[::]
+        self.stack.empty()
+        return result
 
 if __name__ == '__main__':
+    import sys
+    print(sys.version)
     ops = {
-        '+': lambda x, y: x + y,
-        '-': lambda x, y: x - y,
-        '*': lambda x, y: x * y,
-        '/': lambda x, y: x / y,
-        '//': lambda x, y: x // y,
-        '%': lambda x, y: x % y,
-        '^': lambda x, y: x ** y
+        'q': Operator(lambda: None, 0),
+        '+': Operator(lambda x, y: x + y, 2),
+        '-': Operator(lambda x, y: x - y, 2),
+        '*': Operator(lambda x, y: x * y, 2),
+        '/': Operator(lambda x, y: x / y, 2),
+        'sqrt': Operator(lambda x: sqrt(x), 1),
+        'if': Operator(
+            lambda cond_value, if_value, else_value:
+            if_value if cond_value >= 0 else else_value,
+            3
+        )
     }
     calc = RPNCalc(ops)
+    input_stack = Stack()
+    for v in ('sqrt', '*', '+', 1, 2, '-', 6, 3):
+        input_stack.push(v)
+    print(input_stack)
+    print(calc.solve(input_stack))
